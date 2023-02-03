1f0b363147e03e94ae8a635104f632a1f06a57ad
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2023-02-03 11:32:59 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,1 +1,41 @@
+local spy = loadstring(game:HttpGet("https://raw.githubusercontent.com/Error-Cezar/SynSpy/main/source.lua"))()
 
+function Blacklist(remote)
+  local stat = getgenv().SynSpy_Settings.blacklist(remote)
+  if stat then
+    -- success
+  else
+    -- failure
+  end
+end
+
+function Whitelist(remote)
+  local stat = getgenv().SynSpy_Settings.whitelist(remote)
+  if stat then
+    -- success
+  else
+    -- failure
+  end
+end
+
+function get(setting)
+  local setting = getgenv().SynSpy_Settings.get(setting or nil)
+   print(setting or "No Setting Found")
+end
+
+function clearconsole()
+  getgenv().SynSpy_Settings.clear()
+end
+
+function ChangeStatus(Namecall, Value, Category)
+  getgenv().SynSpy_Settings.status(Namecall, Value, Category) -- Not working as of now
+end
+
+function modify_setting(settingName, Value)
+  local stat = getgenv().SynSpy_Settings.modify(settingName, Value)
+    if stat then
+    -- success
+  else
+    -- failure
+  end
+end
